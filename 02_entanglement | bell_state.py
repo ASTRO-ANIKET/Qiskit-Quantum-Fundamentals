49a6a7e35f52ba00145e28e822e36934b43c0b95
ASTRO-ANIKET <aniketshrivastava12345@gmail.com>
2026-03-11 02:03:31 +0530

    Create 02_entanglement | bell_state.py

---
+++ 02_entanglement | bell_state.py
@@ -0,0 +1,27 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
+from qiskit_ibm_runtime import Sampler
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
+# Create circuit
+qc = QuantumCircuit(2)
+
+# Create Bell pair
+qc.h(0)
+qc.cx(0,1)
+
+# Measure
+qc.measure_all()
+print(qc.draw())
+backend = AerSimulator()
+pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+isa_qc = pm.run(qc)
+sampler = Sampler(mode=backend)
+job = sampler.run([isa_qc], shots=1000)
+result = job.result()
+counts = result[0].data.meas.get_counts()
+print(counts)
+plot_histogram(counts)
+
+Expected Output: 00 and 11 only
